2b17b632cff8b8f8a23c5638544b55bc024b6e95
georgehastings <g.hastings3@gmail.com>
2015-09-03 11:40:10 -0400

    finished display view

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 'use strict';
 
-// clientId: 1054522317473-1q9d0u8sd9pqv7ie9cuvv27tsi47q9oo.apps.googleusercontent.com
-// clientSecret: Uj2TcoNu85MPoxkNxmmybmFW
-
 var Events = [];
 var Images = [];
+var Calendars  = {
+	own: 'primary',
+	support: 'ideo.com_3ksmp10u6g268lutghfpb8bkl4@group.calendar.google.com',
+	creativeConnections: 'ideo.com_20hjl85r7mi3e2vtfncskfiabs@group.calendar.google.com',
+	internal: 'ideo.com_3ksmp10u6g268lutghfpb8bkl4@group.calendar.google.com',
+	external: 'ideo.com_20hjl85r7mi3e2vtfncskfiabs@group.calendar.google.com',
+	projects: 'ideo.com_v4vpo5b47up8803v4omofvet4c@group.calendar.google.com'
+};
 
-var tmpl = document.getElementById('event-template');
-var container = document.getElementById('event-container');
+var UI = {
+	tmpl: document.getElementById('event-template'),
+	eventContainer: document.getElementById('event-container')
+};
 
-var Event = function(summary, day, date, time, where, creator, calendar) {
+var Event = function(summary, day, date, time, where, creator, calendar, sortIndex) {
 	this.summary = summary;
 	this.day = day;
 	this.date = date;
@@ -17,11 +24,15 @@ var Event = function(summary, day, date, time, where, creator, calendar) {
 	this.creator = creator;
 	this.where = where;
 	this.calendar = calendar;
+	this.sortIndex = sortIndex;
 };
 
 Event.prototype.getColor = function() {
 	if(this.calendar === 'NY Support') {
-		return '#E56548';
+		return '#EA643B';
+	}
+	if(this.calendar === 'NY - Creative Connections') {
+		return '#4BC4A4';
 	}
 	else {
 		return 'rgb(0,155,255)';
@@ -30,11 +41,13 @@ Event.prototype.getColor = function() {
 
 Event.prototype.handler = function(e) {
 	var obj = JSON.parse(e.target.response);
-	if(obj.results === null) {
-		Images.push('http://newyork.ideo.com/assets/img/Community/Default.jpg');
-	}
-	else {
-		Images.push('http://newyork.ideo.com/'+obj.results[0].image+'');
+	if(obj.results) {
+		var set = document.querySelectorAll('[data-person="'+obj.results[0].email+'"]');
+		console.log(set);
+		for(var i = 0; i < set.length; i++) {
+			set[i].src = ''+obj.results[0].image+'';
+			console.log(set[i].src);
+		}
 	}	
 };
 
@@ -66,27 +79,150 @@ var parseEmail = function(email) {
 	return email.substring(0, email.indexOf('@'));
 };
 
+var sortEventsByTime = function() {
+	Events.sort(function(a, b){
+	  return a.sortIndex-b.sortIndex;
+	});
+};
+
 var renderEvents = function() {
-	console.log(Images);
 	for(var i = 0; i < Events.length; i++) {
 		var thisEvent = Events[i];
-		var template = tmpl.content.cloneNode(true);
-		template.querySelector('.event-day').innerHTML = thisEvent.day;
-		template.querySelector('.event-date').innerHTML = thisEvent.date;
+		var template = UI.tmpl.content.cloneNode(true);
+	
+		if(i === 0 || i > 0 && Events[i-1].day !== thisEvent.day) {
+			template.querySelector('.event-day').innerHTML = thisEvent.day;
+			template.querySelector('.event-date').innerHTML = thisEvent.date;
+		}
+
+		template.querySelector('.event').id = thisEvent.id;
 		template.querySelector('.event-title').innerHTML = thisEvent.summary;
 		template.querySelector('.event-location').innerHTML = thisEvent.where;
 		template.querySelector('.event-time').innerHTML = thisEvent.time;
-		template.querySelector('img').src = Images[i];
+		template.querySelector('img').setAttribute('data-person', thisEvent.creator);
 		template.querySelector('.event-info').style.background = thisEvent.getColor();
-		container.appendChild(template);
+		UI.eventContainer.appendChild(template);
 	}
 };
 
-document.addEventListener('DOMContentLoaded', function(){
-	checkAuth();
-	loadCalendarApi();
-	listUpcomingEvents();
-});
+function getEventDay(thisEvent) {
+  var when;
+  if(thisEvent.start.dateTime) {
+    when = thisEvent.start.dateTime;
+  }
+  else {
+    when = thisEvent.start.date;
+  }
+  return moment(when);
+}
+
+function getDisplayTime(thisEvent) {
+  if(thisEvent.start.dateTime) {
+    var day = moment(thisEvent.start.dateTime).format('ddd');
+    var date = moment(thisEvent.start.dateTime).format('D');
+    var start = moment(thisEvent.start.dateTime);
+    var end = moment(thisEvent.end.dateTime);
+    return [day, date, ''+start.format('h:mm')+' - '+end.format('h:mm a')+''];
+  }
+  else {
+    var when = thisEvent.start.date;
+    var day = moment(when).format('ddd');
+    var date = moment(when).format('D');
+    return [day, date, 'All day'];
+  } 
+}
+
+function getCreator(thisEvent) {
+  var creator;
+  if(thisEvent.creator.email) {
+    creator = thisEvent.creator.email;
+  }
+  else {
+    creator = thisEvent.organizer.email;
+  }
+  return creator;
+}
+
+function getLocation(thisEvent) {
+  var where;
+  if(thisEvent.location) {
+    where = thisEvent.location;
+  }
+  else {
+    where = 'New York';
+  }
+  return where;
+}
+
+function getSortIndex(thisEvent) {
+  var date;
+  if(thisEvent.start.dateTime) {
+    date = moment(thisEvent.start.dateTime);
+  }
+  else {
+    date = moment(thisEvent.start.date);
+  }
+  var sortIndex = date.format('DD') + date.format('HH mm');
+  return parseInt(sortIndex);
+}
+
+function buildEvents(events) {
+    for (var i = 0; i < events.length; i++) {
+	  	var thisEvent = events[i];
+		var summary = thisEvent.summary;
+		var day = getDisplayTime(thisEvent)[0];
+		var date = getDisplayTime(thisEvent)[1];
+		var time = getDisplayTime(thisEvent)[2];
+		var where = getLocation(thisEvent);
+		var creator = getCreator(thisEvent);
+		var calendar = thisEvent.organizer.displayName;
+		var sortIndex = getSortIndex(thisEvent);
+
+		Events.push(new Event(summary, day, date, time, where, creator, calendar, sortIndex));
+	}
+}
+
+var getRequest = function(calendar) {
+  var request = gapi.client.calendar.events.list({
+    'calendarId': calendar,
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 5,
+    'orderBy': 'startTime'
+  });
+  return request;
+};
+
+var listUpcomingPrimaryEvents = function() {
+  getRequest(Calendars.own).execute(function(resp) {
+    buildEvents(resp.items);
+  });
+};
+
+var listUpcomingSupportEvents = function() {
+  getRequest(Calendars.support).execute(function(resp) {
+    buildEvents(resp.items);
+  });
+};
+
+var listUpcomingCreativeConnectionsEvents = function() {
+  getRequest(Calendars.creativeConnections).execute(function(resp) {
+    buildEvents(resp.items);
+  });
+};
+
+function listAllEvents() {
+  listUpcomingPrimaryEvents();
+  listUpcomingSupportEvents();
+  listUpcomingCreativeConnectionsEvents();
+
+  setTimeout(function(){
+  	  sortEventsByTime();
+      renderEvents();
+      fetchImages();
+  }, 2000);
+}
 
 // setInterval(function(){
 // 	location.reload();
